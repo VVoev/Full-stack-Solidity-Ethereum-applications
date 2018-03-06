c31f1a08869bd299348afe08f8b3a72b60375dfe
Vladimir Yonkov Voev <Vladimir.Voev@allocatesoftware.com>
2018-03-06 19:34:59 +0200

    added some basic unit tests

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -6,34 +6,29 @@ const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
+const HELLO = 'Hi There!';
 
-beforeEach(async () => {
-  // Get a list of all accounts
+beforeEach (async ()=>{
   accounts = await web3.eth.getAccounts();
-
-  // Use one of those accounts to deploy
-  // the contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({
-      data: bytecode,
-      arguments: ['Hi there!']
-    })
-    .send({ from: accounts[0], gas: '1000000' });
+  .deploy({data:bytecode,arguments:[HELLO]})
+  .send({from:accounts[0],gas:'1000000'});
+
 });
 
-describe('Inbox', () => {
-  it('deploys a contract', () => {
-    assert.ok(inbox.options.address);
-  });
+describe('Inbox',()=>{
+  it('deploys a contract',()=>{
+  assert.ok(inbox.options.address);
+  })
 
-  it('has a default message', async () => {
-    const message = await inbox.methods.message().call();
-    assert.equal(message, 'Hi there!');
-  });
+  it('has a default message',async ()=>{
+   const message = await inbox.methods.message().call();
+   assert(message, HELLO);
+  })
 
-  it('can change the message', async () => {
-    await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+  it('can change the message',async ()=>{
+    await inbox.methods.setMessage('Testing the setter').send({from:accounts[0]});
     const message = await inbox.methods.message().call();
-    assert.equal(message, 'bye');
-  });
+    assert.equal(message, 'Testing the setter');
+   })
 });
