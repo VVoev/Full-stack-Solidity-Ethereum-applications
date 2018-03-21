f43942a44f58fe196068be25bbc59eb077365ab2
Vladimir Yonkov Voev <Vladimir.Voev@allocatesoftware.com>
2018-03-21 23:07:01 +0200

    lottary

--- lottary-react/src/App.js
+++ lottary-react/src/App.js
@@ -2,20 +2,71 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import web3 from './web3';
+import lottery from './lottery';
 
 class App extends Component {
+  state ={
+    manager:'',
+    players:[],
+    balance:'',
+    value:'',
+    message:'Nothing happend'
+  }
+  handleChange = (e) =>{
+    this.state.value = e.target.value;
+  }
+
+  onSubmitForm = async (e) =>{
+    e.preventDefault();
+    this.setState({message:'Waiting for transaction to happend'});
+    const accouns = await web3.eth.getAccounts();
+    await lottery.methods.enter().send({
+      from:accouns[0],
+      value:web3.utils.toWei(this.state.value,'ether')
+    });
+    this.setState({value:''})
+    this.setState({message:'You have succesfully entered'});
+  };
+
+  handleWinner = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: 'Waiting on transaction success...' });
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    });
+
+    this.setState({ message: 'A winner has been picked!' });
+  };
+
+  async componentDidMount(){
+    const manager = await lottery.methods.manager().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    const players = await lottery.methods.getPlayers().call();
+    this.setState({manager,balance, players});
+  }
   render() {
-    console.log(web3);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+     <div>
+       <h2>Lottery Contract</h2>
+       <p>This contract is managed by {this.state.manager}</p>
+        <p>These are currently total of {this.state.players.length} players</p>
+        <p>Competing for {web3.utils.fromWei(this.state.balance)} ethers</p>
+        <hr/>
+        <form onSubmit={this.onSubmitForm}>
+        <h4>Want to try your luck</h4>
+        <label>Amount of ethers to send</label>
+        <input onChange={this.handleChange} />
+        <br/>
+        <button>Enter</button>
+        </form>
+        <h1>{this.state.message}</h1>
+
+        <br/>
+        <h4>Pick a winner</h4>
+        <button onClick={this.handleWinner}>Find a Winner</button>
+     </div>
     );
   }
 }
